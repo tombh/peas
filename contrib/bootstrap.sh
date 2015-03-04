140923d1c1a20a759ba0a1766127c3ec841adadd
Thomas Buckley-Houston <tom@tombh.co.uk>
2015-03-04 12:27:19 +0000

    In bootstrap.sh use printf 'y\n' instead of bash's yes command. Touches #28

--- contrib/bootstrap.sh
+++ contrib/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Install `pacapt', a wrapper around popular *nix package managers
 # See; https://github.com/icy/pacapt
@@ -12,7 +13,7 @@ pacapt -Sy
 docker=$(pacapt -Ss docker | grep -i container | head -1 | awk '{print $1;}')
 
 # Install Docker. Crudely bombard the installation with 'y' in case there are any user prompts
-yes | pacapt -S $docker
+printf 'y\ny\ny\ny\ny\n' | pacapt -S $docker
 
 # Setup and run the Peas DinD container to run the API on port 80 and Git server on port 22
 curl -sSL https://raw.githubusercontent.com/tombh/peas/master/contrib/peas-dind/run.sh | sh -s 80 22
